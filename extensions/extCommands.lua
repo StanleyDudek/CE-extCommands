947126a5765c71fce61f9a04c45df20fc75259f0
Dudekahedron <49531350+StanleyDudek@users.noreply.github.com>
2021-01-13 10:22:17 -0500

    Update extCommands.lua

--- extensions/extCommands.lua
+++ extensions/extCommands.lua
@@ -27,14 +27,14 @@ local extCommands =
 {
 	--orginModule[commandName] is where the command is executed from
 	-- Source-Limit-Map [0:no limit | 1:Chat Only | 2:RCON Only]
-	clear =			{orginModule = "extCommands",	level = 1,	arguments = 0,	sourceLimited = 0,	description = "Deletes all of your vehicles"},
-	c =				{orginModule = "extCommands",	level = 1,	arguments = 0,	sourceLimited = 0,	description = "Deletes all of your vehicles"},
-	nuke =			{orginModule = "extCommands",	level = 10,	arguments = 0,	sourceLimited = 0,	description = "Deletes all vehicles on the server"},
-	n =				{orginModule = "extCommands",	level = 10,	arguments = 0,	sourceLimited = 0,	description = "Deletes all vehicles on the server"},
+	clear =			{orginModule = "extCommands",	level = 1,	arguments = 0,					sourceLimited = 0,	description = "Deletes all of your vehicles"},
+	c =				{orginModule = "extCommands",	level = 1,	arguments = 0,					sourceLimited = 0,	description = "Deletes all of your vehicles"},
+	nuke =			{orginModule = "extCommands",	level = 10,	arguments = 0,					sourceLimited = 0,	description = "Deletes all vehicles on the server"},
+	n =				{orginModule = "extCommands",	level = 10,	arguments = 0,					sourceLimited = 0,	description = "Deletes all vehicles on the server"},
 	pop =			{orginModule = "extCommands",	level = 10,	arguments = {"target","vehID"},	sourceLimited = 0,	description = "Deletes a specific targeted vehicle of a player"},
 	p =				{orginModule = "extCommands",	level = 10,	arguments = {"target","vehID"},	sourceLimited = 0,	description = "Deletes a specific targeted vehicle of a player"},
-	popall =		{orginModule = "extCommands",	level = 10,	arguments = {"target"},	sourceLimited = 0,	description = "Deletes all of a target player's vehicles"},
-	pa =			{orginModule = "extCommands",	level = 10,	arguments = {"target"},	sourceLimited = 0,	description = "Deletes all of a target player's vehicles"},
+	popall =		{orginModule = "extCommands",	level = 10,	arguments = {"target"},		sourceLimited = 0,	description = "Deletes all of a target player's vehicles"},
+	pa =			{orginModule = "extCommands",	level = 10,	arguments = {"target"},			sourceLimited = 0,	description = "Deletes all of a target player's vehicles"},
 
 }
 
@@ -44,15 +44,19 @@ applyCommands(commands, extCommands)
 --removes the command-user's vehicles
 local function clear(player, ...)
 	if player.playerID ~= nil then
-		vehCount = -1
-		for k,v in pairs(player.vehicles) do
-			vehCount = vehCount + 1
+		if players[player].vehicles ~= nil then
+			vehCount = -1
+			for k,v in pairs(player.vehicles) do
+				vehCount = vehCount + 1
+			end
+			for vehID = 0, vehCount do
+				RemoveVehicle(player.playerID,vehID)
+			end
+			CElog(player.name .. " cleared their vehicles", "extCommands")
+			return "You have cleared your vehicles"
+		else
+			return "Nothing to clear!"
 		end
-		for vehID = 0, vehCount do
-			RemoveVehicle(player.playerID,vehID)
-		end
-		CElog(player.name .. " cleared their vehicles")
-		return "You have cleared your vehicles"
 	else
 		return "Nothing to clear, are you RCON?"
 	end
@@ -61,13 +65,13 @@ end
 --removes a specific vehicle of the target
 local function pop(player, target, vehID, ...)
 
-	if target ~= nil then
-		if vehID ~= nil then
+	if players[target] ~= nil then
+		if players[target].vehicles ~= nil then
 			RemoveVehicle(target,vehID)
 			SendChatMessage(target, "Your vehicle has been removed")
 			return "player's vehicle has been popped"
 		else
-			return "Vehicle not found, check vehID"
+			return "Vehicles not found, check players[target].vehicles"
 		end
 	else
 		return "Player not present, check playerID"	
@@ -77,16 +81,20 @@ end
 
 --removes all the vehicles of the target
 local function popall(player, target, ...)
-	if target ~= nil then
+	if players[target] ~= nil then
 		vehCount = -1
-		for k,v in pairs(players[tonumber(target)].vehicles) do
-			vehCount = vehCount + 1
+		if players[target].vehicles ~= nil then
+			for k,v in pairs(players[tonumber(target)].vehicles) do
+				vehCount = vehCount + 1
+			end
+			for vehID = -0, vehCount do
+				RemoveVehicle(target,vehID)
+				SendChatMessage(target, "Your vehicle has been removed")
+			end
+			return "You popped 'em all"
+		else
+			return "Target has no vehicles"
 		end
-		for vehID = -0, vehCount do
-			RemoveVehicle(target,vehID)
-			SendChatMessage(target, "Your vehicle has been removed")
-		end
-		return "You popped 'em all"
 	else
 		return "Player not present, check playerID"
 	end
@@ -106,12 +114,6 @@ local function nuke(player, ...)
 		end
 	end
 	SendChatMessage(-1, "Everyone's vehicles have been removed")
-	--handle RCON nuke
-	if player.name then
-		CElog(player.name .. " nuked the server")
-	else
-		CElog(player.ID .. " nuked the server")
-	end
 	return "Server has been nuked"
 end
 
